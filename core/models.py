269412a31b2f63ed9283099bfc40cadae9e0387c
pouriashiralipour <pouriashiralipour@gmail.com>
2024-11-04 11:58:30 +0330

    add customuser to admin panel

--- core/models.py
+++ core/models.py
@@ -4,7 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    username = None
-    phone_number = models.CharField(_("phone_number"), max_length=11)
-    USERNAME_FIELD = "phone_number"
-    REQUIRED_FIELDS = ["phone_number"]
+    pass
+
