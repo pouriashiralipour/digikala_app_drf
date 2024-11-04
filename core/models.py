d6533d6921ea5aef2f6b769f56481ee2745b9c56
pouriashiralipour <pouriashiralipour@gmail.com>
2024-11-04 11:51:05 +0330

    create customuser

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    phone_number = models.CharField(_("phone_number"), max_length=11)
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["phone_number"]
