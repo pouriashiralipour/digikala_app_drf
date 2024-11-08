0e421ade2e48d8113a47ebd31189889d2240a668
pouriashiralipour <pouriashiralipour@gmail.com>
2024-11-08 22:42:45 +0330

    add products model to store

--- store/models.py
+++ store/models.py
@@ -1,3 +1,153 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Color(models.Model):
+    color_name = models.CharField(max_length=30, verbose_name=_("color_name"))
+    color_code = models.CharField(max_length=30, verbose_name=_("color_code"))
+
+    class Meta:
+        verbose_name = _("Color")
+        verbose_name_plural = _("Colors")
+
+    def __str__(self):
+        return self.color_name
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=50, verbose_name=_("title"), default="Apple")
+    slug = models.SlugField(
+        max_length=255, verbose_name=_("slug"), unique=True, allow_unicode=True
+    )
+    english_title = models.CharField(
+        max_length=50, verbose_name=_("english title"), default="Apple"
+    )
+    image = models.ImageField(
+        upload_to="brand",
+        verbose_name=_("image"),
+    )
+
+    class Meta:
+        verbose_name = _("Barnd")
+        verbose_name_plural = _("Barnds")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    parent = models.ForeignKey(
+        "self",
+        default=None,
+        null=True,
+        blank=True,
+        verbose_name=_("parent"),
+        on_delete=models.SET_NULL,
+        related_name="child",
+    )
+    title = models.CharField(max_length=255, verbose_name=_("title"))
+    english_title = models.CharField(
+        max_length=255, verbose_name=_("english title"), blank=True
+    )
+    slug = models.SlugField(
+        max_length=255, verbose_name=_("slug"), unique=True, allow_unicode=True
+    )
+    image = models.ImageField(
+        upload_to="category", verbose_name=_("image"), blank=True, null=True
+    )
+    cover = models.ImageField(
+        upload_to="category", verbose_name=_("cover"), blank=True, null=True
+    )
+    is_parent = models.BooleanField(
+        default=False, blank=True, null=True, verbose_name=_("is_parent")
+    )
+    is_offer = models.BooleanField(
+        default=False, blank=True, null=True, verbose_name=_("is offer")
+    )
+
+    datetime_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("datetime created")
+    )
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    STATUS = {
+        ("draft", "Draft"),
+        ("in_review", "In Review"),
+        ("published", "Published"),
+    }
+    title = models.CharField(_("product"), max_length=255)
+    english_title = models.CharField(
+        max_length=255, verbose_name=_("english title"), null=True, blank=True
+    )
+    slug = models.SlugField(
+        max_length=255, verbose_name=_("slug"), unique=True, allow_unicode=True
+    )
+    category = models.ManyToManyField(
+        Category, verbose_name=_("category"), related_name="products"
+    )
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.PROTECT,
+        null=True,
+        verbose_name=_("brand"),
+        related_name="products",
+    )
+    color = models.ManyToManyField(
+        Color, verbose_name=_("color"), related_name="products"
+    )
+    image = models.ImageField(
+        upload_to="products/",
+        verbose_name=_("image"),
+    )
+    description = models.TextField(_("description"))
+    price = models.PositiveBigIntegerField(verbose_name=_("price"))
+    old_price = models.PositiveBigIntegerField(verbose_name=_("old_price"))
+    status = models.CharField(
+        choices=STATUS,
+        max_length=10,
+        verbose_name=_("status"),
+        default="in_review",
+    )
+    datetime_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("datetime created")
+    )
+    datetime_modified = models.DateTimeField(
+        auto_now=True, verbose_name=_("datetime modified")
+    )
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    def get_precentage(self):
+        new_price = 100 - ((self.price / self.old_price) * 100)
+        return new_price
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImages(models.Model):
+    images = models.ImageField(upload_to="product-image", verbose_name=_("images"))
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name=_("product"),
+        related_name="product_images",
+    )
+    datetime_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("datetime created")
+    )
+
+    class Meta:
+        verbose_name = _("ProductImage")
+        verbose_name_plural = _("ProductImages")
